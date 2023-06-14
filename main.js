621430c8185170c091f83c7acc1b6685f52c8d9f
yalmuhtrish <yalmuhtrish@elm.sa>
2023-06-14 15:28:54 +0300

    adding other files

---
+++ main.js
@@ -0,0 +1,24 @@
+console.log("start main");
+
+const crazyValue  = {name : {name :  ()=>()=>(w)=> w?"":{whateever : "crazy value"} }}; // = 0
+console.log(crazyValue.name.name()()(false).whateever)
+// let a = ()=>{
+//     return "helle"
+// }
+
+// let q = () => "helle"
+
+
+// const s  = function(){
+//     return "helle"
+// }
+// if(crazyValue){
+//     console.log("true ");
+// }else{
+//     console.log("false ");
+// }
+let a="";
+
+console.log(crazyValue.name.name()()(a)) // lujain
+console.log(crazyValue.name.name.whateever); // omar
+console.log(crazyValue.name.name(false))
